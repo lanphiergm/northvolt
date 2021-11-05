96f42b941ff20df6aa1f5f1184b689e0febb7ebd
lanphiergm <lanphiergm@gmail.com>
2021-11-05 19:45:28 -0400

    Initial working revision

---
+++ Northvolt/Controllers/KeyValuePairController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Northvolt.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Northvolt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KeyValuePairController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, double>> Get() => KeyValuePairModel.GetList();
+
+        [HttpPost]
+        public void Add(string key, double value) => KeyValuePairModel.Add(key, value);
+    }
+}
---
+++ Northvolt/Models/KeyValuePairModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Northvolt.Models
+{
+    public static class KeyValuePairModel
+    {
+        private static readonly Dictionary<string, (double value, DateTime timestamp)> _backingStore = new();
+
+        public static IEnumerable<KeyValuePair<string, double>> GetList() => 
+            _backingStore.Select(kvp => new KeyValuePair<string, double>(kvp.Key, kvp.Value.value));
+
+        public static void Add(string key, double value) => _backingStore.Add(key, (value, DateTime.Now));
+    }
+}
